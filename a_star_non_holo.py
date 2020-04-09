bda057f0458005ecb04e5853db4a1cc110d0e3a3
prachuu <pradeepprachu@gmail.com>
2020-04-09 16:23:37 -0400

    Adding the video result and the final version of the code

--- a_star_non_holo.py
+++ a_star_non_holo.py
@@ -1,4 +1,4 @@
-import cv2
+# import cv2
 import numpy as np
 import time
 import math
@@ -100,7 +100,7 @@ def threshold(x, y, th, theta):
     return (x, y, th)
 
 
-def plot_curve(Xi, Yi, Thetai, UL, UR, r, c, flag):
+def plot_curve(Xi, Yi, Thetai, UL, UR, r, c, flag, N_list, S_list):
     t = 0
     r = 0.038
     L = 0.354
@@ -123,22 +123,21 @@ def plot_curve(Xi, Yi, Thetai, UL, UR, r, c, flag):
         Thetan += (r / L) * (UR - UL) * dt
         if not (obstacle_check(Xn, Yn, r, c)):
             if flag == 0:
-                plt.plot([Xs, Xn], [Ys, Yn], color="blue")
+                # plt.plot([Xs, Xn], [Ys, Yn], color="blue")
                 cost = cost + cost2go((Xs, Ys), (Xn, Yn))
+                N_list.append((Xn, Yn))
+                S_list.append((Xs, Ys))
             if flag == 1:
                 plt.plot([Xs, Xn], [Ys, Yn], color="red")
         else:
             return None
-
     Thetan = 180 * (Thetan) / 3.14
-    return [Xn, Yn, Thetan, cost]
+    return [Xn, Yn, Thetan, cost, N_list, S_list]
 
 
-def a_star(start_node, goal_node, step_size, theta, rad, clearance):
-    x_explored = []
-    y_explored = []
-    x_parent = []
-    y_parent = []
+def a_star(start_node, goal_node, step_size, theta, rad, clearance, L_rpm, R_rpm):
+    N_list = []
+    S_list = []
 
     start_node = threshold(start_node[0], start_node[1], start_node[2], theta)
 
@@ -147,9 +146,9 @@ def a_star(start_node, goal_node, step_size, theta, rad, clearance):
     #     print(goal_node)
     visited_nodes = np.zeros((140, 140, int(360 / theta)))
     start = (0, start_node, None, None)  # cost, node, parent node
-    print("start", start)
+    # print("start", start)
     goal = (0, goal_node, None, None)
-    print("goal", goal)
+    # print("goal", goal)
 
     nodes = queue.PriorityQueue()
     path_nodes = []
@@ -164,35 +163,14 @@ def a_star(start_node, goal_node, step_size, theta, rad, clearance):
             current_node[0] = current_node[0] - cost2go(current_node[1], goal[1])
             action_dict[current_node[2]] = current_node[3]
 
-        #             str1 = str(current_node[1][0])
-        #             str2 = str(current_node[1][1])
-        #             str3 = str(current_node[1][2])
-
-        #             str4 = str(current_node[2][0])
-        #             str5 = str(current_node[2][1])
-        #             str6 = str(current_node[2][2])
-
-        #             str_node = str1+','+str2+','+str3
-        #             str_parent = str4+','+str5+','+str6
 
         path_dict[current_node[1]] = current_node[2]
 
-        #         else:
-        # #             str1 = str(current_node[1][0])
-        # #             str2 = str(current_node[1][1])
-        # #             str3 = str(current_node[1][2])
-        # #             str4 = str(current_node[2])
-
-        # #             str_node = str1+','+str2+','+str3
-        # #             str_parent = str4
-        #             path_dict[current_node[1]] = None
-        #             action_dict[current_node[1]] = None
-
-        actions = action_model(8, 10)
+        actions = action_model(L_rpm, R_rpm)
 
         for new_pos in actions:
             X1 = plot_curve(current_node[1][0], current_node[1][1], current_node[1][2], new_pos[0], new_pos[1], rad,
-                            clearance, 0)
+                            clearance, 0, N_list, S_list)
 
             if (X1 != None):
                 angle = X1[2]
@@ -234,10 +212,10 @@ def a_star(start_node, goal_node, step_size, theta, rad, clearance):
             print("Goal reached")
 
             # print("path_nodes = ", path_nodes)
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
-            print('Goal reached')
-            print("Time taken to explore = ", current_time)
+            # t = time.localtime()
+            # current_time = time.strftime("%H:%M:%S", t)
+            # print('Goal reached')
+            # print("Time taken to explore = ", current_time)
             path = []
             path.append((goal_node[0], goal_node[1]))
 
@@ -251,42 +229,15 @@ def a_star(start_node, goal_node, step_size, theta, rad, clearance):
 
             while parent != None:
                 temp = path_dict.get(parent)
-                #                 print(parent)
-                #                 if(parent[1]=='.' and parent[5]=='.'):
-                #                     par_1 = float(parent[0])+float(parent[2])/10
-                #                     par_2 = float(parent[4])+float(parent[6])/10
-                #                 if(parent[2]=='.' and parent[7]=='.'):
-                #                     par_1 = float(parent[0]+parent[1])+float(parent[3])/10
-                #                     par_2 = float(parent[5]+parent[6])+float(parent[8])/10
-                #                 if(parent[1]=='.' and parent[6]=='.'):
-                #                     par_1 = float(parent[0])+float(parent[2])/10
-                #                     par_2 = float(parent[4]+parent[5])+float(parent[7])/10
-                #                 if(parent[2]=='.' and parent[6]=='.'):
-                #                     par_1 = float(parent[0]+parent[1])+float(parent[3])/10
-                #                     par_2 = float(parent[5])+float(parent[7])/10
-                #                 if(parent[3]=='.' and parent[9]=='.'):
-                #                     par_1 = float(parent[0]+parent[1]+parent[2])+float(parent[4])/10
-                #                     par_2 = float(parent[6]+parent[7]+parent[8])+float(parent[10])/10
-                #                 if(parent[3]=='.' and parent[7]=='.'):
-                #                     par_1 = float(parent[0]+parent[1]+parent[2])+float(parent[4])/10
-                #                     par_2 = float(parent[6])+float(parent[8])/10
-                #                 if(parent[3]=='.' and parent[8]=='.'):
-                #                     par_1 = float(parent[0]+parent[1]+parent[2])+float(parent[4])/10
-                #                     par_2 = float(parent[6]+parent[7])+float(parent[9])/10
-                #                 if(parent[1]=='.' and parent[7]=='.'):
-                #                     par_1 = float(parent[0])+float(parent[2])/10
-                #                     par_2 = float(parent[4]+parent[5]+parent[6])+float(parent[8])/10
-                #                 if(parent[2]=='.' and parent[8]=='.'):
-                #                     par_1 = float(parent[0]+parent[1])+float(parent[3])/10
-                #                     par_2 = float(parent[5]+parent[6]+parent[7])+float(parent[9])/10
+
                 path.append(parent)
                 #                 print("path",path)
                 parent = temp
                 if (parent == (start_node[0], start_node[1])):
                     break
-            t = time.localtime()
-            current_time = time.strftime("%H:%M:%S", t)
-            print("time taken to backtrack = ", current_time)
+            # t = time.localtime()
+            # current_time = time.strftime("%H:%M:%S", t)
+            # print("time taken to backtrack = ", current_time)
             plt.plot(start_node[0], start_node[1], color='green', marker='o', linestyle='dashed', linewidth=1,
                      markersize=4)
             #             path.append((start_node[0], start_node[1]))
@@ -294,70 +245,62 @@ def a_star(start_node, goal_node, step_size, theta, rad, clearance):
 
             path = path[::-1]
             print(path)
+            # print(N_list, S_list)
 
-            #             X7 = [start_node[0], start_node[1], start_node[2]]
-            #             for p in path:
-            #                 temp1 = action_dict.get(p)
-            #                 print("temp1",temp1)
-            #                 print("p", p)
-            #                 X7 = plot_curve(X7[0], X7[1], X7[2], p[0], p[1], rad, clearance, 1)
-            #                 print("x2",X7 )
-            #             print(action_dict)
-            #             acts = []
-            #             for p in path:
-
-            #                 temp1 = action_dict.get(p)
-            #                 print("p", p)
-            #                 print("temp1", temp1)
-            #                 acts.append(temp1)
-
-            #             X2 = [start_node[0], start_node[1], start_node[2]]
-            #             print("acts", acts)
-            #             for x in acts:
-            #                 print("x", x)
-            #                 print("X2",X2)
-            #                 X2= plot_curve(X2[0],X2[1],X2[2],x[0], x[1],rad,clearance, 1)
-            return path
+            return path, N_list, S_list
 
 
 def main():
-    #     Parser = argparse.ArgumentParser()
-    #     Parser.add_argument('--user_input')
+    Parser = argparse.ArgumentParser()
+    Parser.add_argument('--user_input')
 
-    #     Args = Parser.parse_args()
-    #     user_input = int(Args.user_input)
-    #     if user_input == 1:
-    #         x_start = float(input('Enter the x-coordinate of start point: '))
-    #         y_start = float(input('Enter the y-coordinate of start point: '))
-    #         theta_start = float(input('Enter the orientation of start point: '))
-    #         goal_x = float(input('Enter the x-coordinate of goal point: '))
-    #         goal_y = float(input('Enter the y-coordinate of goal point: '))
-    #         robot_radius = float(input('Enter the radius of the robot: '))
-    #         clearance = float(input('Enter the desired clearance : '))
-    #         theta = float(input('Enter the angle between the action at each node'))
-    #         step_size = float(input('Enter the step size'))
-    #         theta_goal = 0.0
+    Args = Parser.parse_args()
+    user_input = int(Args.user_input)
+    if user_input == 1:
+        x_start = float(input('Enter the x-coordinate of start point: '))
+        y_start = float(input('Enter the y-coordinate of start point: '))
+        theta_start = float(input('Enter the orientation of start point: '))
+        x_goal = float(input('Enter the x-coordinate of goal point: '))
+        y_goal = float(input('Enter the y-coordinate of goal point: '))
 
-    #     else:
+        L_rpm = float(input('Enter the left wheel RPM: '))
+        R_rpm = float(input('Enter the right wheel RPM: '))
+        clearance1 = float(input('Enter the clearance for the robot: '))
 
-    x_start = 0.5
-    y_start = 0.5
-    theta_start = 0.0
-    theta = 15
-    # (1.333226644398235, 1.210572252906252, 67.6886537838713)
-    #     x_goal = 5.18
-    #     y_goal = 0.5
-    x_goal = 9.5
-    y_goal = 9.5
-    theta_goal = 0.0
+        robot_radius = 0.105
+        clearance = 0.16 + clearance1
+        theta = 15
+        step_size = 1
+        theta_goal = 0.0
 
-    robot_radius = 0.038
-    clearance = 0.25
-    step_size = 1.0
+    else:
 
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    print("start_time = ", current_time)
+        x_start = -4.5
+        y_start = -4.5
+        theta_start = 0.0
+        theta = 15
+        x_goal = 4.5
+        y_goal = 4.5
+        theta_goal = 0.0
+        step_size = 1
+
+        robot_radius = 0.033
+        clearance = 0.16
+
+        L_rpm = 9.0
+        R_rpm = 11.0
+
+
+    print("start_coordinates = ", x_start, y_start)
+    print("goal_coordinates = ", x_goal, y_goal)
+    x_start += 5.0
+    y_start += 5.0
+    x_goal += 5.0
+    y_goal += 5.0
+    # t = time.localtime()
+    # current_time = time.strftime("%H:%M:%S", t)
+    # print("start_time = ", current_time)
+    start_time =  time.time()
     start_node = (x_start, y_start, theta_start)
     goal_node = (x_goal, y_goal, theta_goal)
     if obstacle_check(start_node[0], 10 - start_node[1], robot_radius, clearance) == True:
@@ -367,7 +310,7 @@ def main():
     else:
 
         fig, ax = plt.subplots()
-        ax.set(xlim=(0, 10), ylim=(0, 10))
+        ax.set(xlim=(0, 10.2), ylim=(0, 10.2))
 
         #         plot_x, plot_y, rigid_x, rigid_y = obstacle_map(robot_radius, clearance)
 
@@ -390,7 +333,7 @@ def main():
         ax.add_artist(c4)
         ax.set_aspect('equal')
         plt.grid()
-        path = a_star(start_node, goal_node, step_size, theta, robot_radius, clearance)
+        path, N_list, S_list = a_star(start_node, goal_node, step_size, theta, robot_radius, clearance, L_rpm, R_rpm)
 
         #         path,x_explored,y_explored,x_parent,y_parent = a_star(start_node, goal_node, step_size, theta, robot_radius, clearance)
 
@@ -398,26 +341,29 @@ def main():
             print("Path could not be found. Check inputs")
 
         else:
-
-            # #             l = 0
-            # #             while l < len(x_explored):
-            # #                 plt.plot([x_explored[l], x_parent[l]], [y_explored[l], y_parent[l]], "-c")
-            # #                 l = l + 1
-            # #                 plt.show()
-            # #                 plt.pause(0.000000000000000000000000000000000005)
+            end_time = time.time()
+            print("Time taken to find the shortest path (in seconds) is:", abs(end_time - start_time))
+            l = 0
+            while l < len(N_list):
+                # plt.plot([Xs, Xn], [Ys, Yn], color="blue")
+                plt.plot([S_list[l][0], N_list[l][0]], [S_list[l][1], N_list[l][1]], color="blue")
+                # plt.plot([x_explored[l], x_parent[l]], [y_explored[l], y_parent[l]], "-c")
+                l = l + 1
+                plt.show()
+                plt.pause(0.000000000000000000000000000000000005)
 
             path = path[::-1]
             x_path = [path[i][0] for i in range(len(path))]
             y_path = [path[i][1] for i in range(len(path))]
             plt.plot(x_path, y_path, "-r")
-    plt.figure(figsize=(10, 10))
-    plt.show()
+    # plt.figure(figsize=(10, 10))
+    # plt.show()
     plt.savefig("mygraph.png")
     plt.pause(5)
     plt.close()
     t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    print("Time taken to plot = ", current_time)
+    end_time2 = time.time()
+    print("Time taken to find the shortest path and to plot (in seconds) is:", abs(end_time2 - start_time))
 
 
 if __name__ == '__main__':
